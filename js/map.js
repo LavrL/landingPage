f36e28d1bd71e085a1ffb7c484bbf40054d24014
LavrL <llembersk@gmail.com>
2020-01-29 00:08:36 +0200

    configured webpack

--- js/map.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function () {
-    var elem = document.createElement('script');
-    elem.type = 'text/javascript';
-    elem.src = '//api-maps.yandex.ru/2.1/?apikey=60da896e-65e1-445c-8072-0a9654b98584&lang=ru_RU';
-}, 2000);
